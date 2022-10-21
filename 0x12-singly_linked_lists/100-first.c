96a2a396c107c61650c5df798c9b0c5b2ff137d0
Holutunde <Holutunde99@gmail.com>
2022-10-21 21:58:40 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * Start_up - print before main
+ * Description: print a string before the main
+ * Return: 0
+ */
+void __attribute__((constructor)) Start_up()
+
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
